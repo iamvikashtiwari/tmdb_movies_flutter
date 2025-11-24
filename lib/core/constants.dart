e7f064fe1ffa92e23e16ee634d5967d25ab06dd7
Vikash <tiwarivikash2302@gmail.com>
2025-11-24 16:22:06 +0530

    API key removed

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const String apiKey = "e2e504a1c8e46d8206ca7b69b6738fbf";
+  static const String apiKey = ""; //Place your api key here 
   static const String baseUrl = "https://api.themoviedb.org/3";
   static const String imageBase = "https://image.tmdb.org/t/p/w500/";
 }
